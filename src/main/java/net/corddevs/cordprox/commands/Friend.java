3c1719af39da62a7087d387391bfb42e0d35247c
DaatUserName <40370496+DaatUserName@users.noreply.github.com>
2020-12-05 15:13:40 +0100

    Friend system

--- src/main/java/net/corddevs/cordprox/commands/Friend.java
+++ src/main/java/net/corddevs/cordprox/commands/Friend.java
@@ -1,6 +1,7 @@
 package net.corddevs.cordprox.commands;
 
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class Friend extends Command {
@@ -11,6 +12,11 @@ public class Friend extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-
+        if(sender instanceof ProxiedPlayer) {
+            ProxiedPlayer p = (ProxiedPlayer) sender;
+            if(args.length == 0) {
+                return;
+            }
+        }
     }
 }
